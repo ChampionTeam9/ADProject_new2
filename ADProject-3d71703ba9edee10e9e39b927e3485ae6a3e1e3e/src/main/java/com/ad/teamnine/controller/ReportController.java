0a37bdeda94a214d3a7620c7d2aa74bcce98aff8
Lindsayyy-w <Lindsayyyw@hotmail.com>
2024-02-05 16:40:38 +0800

    add report and some I don't know

--- ADProject-3d71703ba9edee10e9e39b927e3485ae6a3e1e3e/src/main/java/com/ad/teamnine/controller/ReportController.java
+++ ADProject-3d71703ba9edee10e9e39b927e3485ae6a3e1e3e/src/main/java/com/ad/teamnine/controller/ReportController.java
@@ -1,9 +1,85 @@
 package com.ad.teamnine.controller;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.ad.teamnine.model.Member;
+import com.ad.teamnine.model.MemberReport;
+import com.ad.teamnine.model.RecipeReport;
+import com.ad.teamnine.model.Report;
+import com.ad.teamnine.service.RecipeService;
+import com.ad.teamnine.service.ReportService;
+import com.ad.teamnine.service.UserService;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/report")
 public class ReportController {
+	
+	@Autowired
+	private ReportService reportService;
+	
+	@Autowired
+	private RecipeService recipeService;
+	
+	@Autowired
+	private UserService userService;
+	
+	//report recipes
+	@GetMapping("/reportRecipe/{recipeId}")
+	public String getReportRecipe(@PathVariable(value = "recipeId") Integer recipeId,
+			Model model,
+			HttpSession sessionObj) {
+		RecipeReport report = new RecipeReport();
+		Member member = 
+			userService.getMemberById((int)sessionObj.getAttribute("userId"));
+//		Member member =userService.getMemberById(1533);
+		model.addAttribute("report",report);
+		model.addAttribute("recipe",recipeService.getRecipeById(recipeId));
+		model.addAttribute("member",member);
+		return "ReportViews/showRecipeReportPage";
+	}
+	
+	@PostMapping("/reportRecipe")
+	public String reportRecipe(@ModelAttribute("report") RecipeReport report) {
+		
+		reportService.reportRecipes(report);
+		Integer recipeId = report.getRecipeReported().getId();
+		return "redirect:/recipe/view/"+recipeId;
+		
+	}
+	
+	@GetMapping("/reportMember/{memberId}")
+	public String getReportMember(@PathVariable Integer memberId,
+			Model model,
+			HttpSession sessionObj) {
+		MemberReport report = new MemberReport();
+		Member member = 
+				userService.getMemberById((int)sessionObj.getAttribute("userId"));
+		model.addAttribute("report",report);
+		model.addAttribute("memberReported",userService.getMemberById(memberId));
+		model.addAttribute("member",member);
+		return "ReportViews/showMemberReportPage";
+	}
+//	
+//	@PostMapping("/reportMember")
+//	public String reportMember(@ModelAttribute("report") MemberReport report) {
+//		
+//		reportService.reportMembers(report);
+//		Member author = report.getMember();
+//		
+//		
+//		return "redirect:/recipe/view/"+recipeId;
+//		
+//	}
+	
+
 
 }
