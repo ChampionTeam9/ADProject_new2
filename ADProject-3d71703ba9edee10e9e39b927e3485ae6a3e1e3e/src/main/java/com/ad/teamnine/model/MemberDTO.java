4201767a7b0b90879cfd731cbb25f97b9192304f
BlueEyesAndWhiteDragon <e1285209@u.nus.edu>
2024-02-16 12:11:56 +0800

    add username

--- ADProject-3d71703ba9edee10e9e39b927e3485ae6a3e1e3e/src/main/java/com/ad/teamnine/model/MemberDTO.java
+++ ADProject-3d71703ba9edee10e9e39b927e3485ae6a3e1e3e/src/main/java/com/ad/teamnine/model/MemberDTO.java
@@ -4,7 +4,30 @@ import java.time.LocalDate;
 
 public class MemberDTO {
     private String username;
+	private Double height;
+    private Double weight;
+    private Integer age;
+    private LocalDate birthdate;
+    private String gender;
+    private Double calorieIntake;
+    private LocalDate registrationDate;
     
+
+    public MemberDTO() {}
+
+    
+    public MemberDTO(Member member) {
+        this.username = member.getUsername();
+        this.height = member.getHeight();
+        this.weight = member.getWeight();
+        this.age = member.getAge();
+        this.birthdate = member.getBirthdate();
+        this.gender = member.getGender();
+        this.calorieIntake = member.getCalorieIntake();
+        this.registrationDate = member.getRegistrationDate();
+        
+    }
+
     public String getUsername() {
 		return username;
 	}
@@ -68,31 +91,4 @@ public class MemberDTO {
 	public void setRegistrationDate(LocalDate registrationDate) {
 		this.registrationDate = registrationDate;
 	}
-
-	private Double height;
-    private Double weight;
-    private Integer age;
-    private LocalDate birthdate;
-    private String gender;
-    private Double calorieIntake;
-    private LocalDate registrationDate;
-    // 可以根据需要添加其他字段
-
-    public MemberDTO() {}
-
-    // 添加一个构造函数，用于从Member对象创建DTO对象
-    public MemberDTO(Member member) {
-        this.username = member.getUsername();
-        this.height = member.getHeight();
-        this.weight = member.getWeight();
-        this.age = member.getAge();
-        this.birthdate = member.getBirthdate();
-        this.gender = member.getGender();
-        this.calorieIntake = member.getCalorieIntake();
-        this.registrationDate = member.getRegistrationDate();
-        // 如果需要的话，可以继续将其他字段从Member对象复制到DTO对象
-    }
-
-    // 添加getter和setter方法
-    // ...
 }
--- ADProject-3d71703ba9edee10e9e39b927e3485ae6a3e1e3e/src/main/java/com/ad/teamnine/model/RecipeDTO.java
+++ ADProject-3d71703ba9edee10e9e39b927e3485ae6a3e1e3e/src/main/java/com/ad/teamnine/model/RecipeDTO.java
@@ -3,6 +3,8 @@ package com.ad.teamnine.model;
 import java.time.LocalDate;
 import java.util.List;
 
+
+
 public class RecipeDTO {
 	private Integer id;
 	private String name;
@@ -16,13 +18,23 @@ public class RecipeDTO {
 	private Integer servings;
 	private Integer preparationTime;
 	private List<String> steps;
-
+	private Integer healthScore;
+	private Double calories;
+	private Double protein;
+	private Double carbohydrate;
+	private Double sugar;
+	private Double sodium;
+	private Double fat;
+	private Double saturatedFat;
+	private String username;
+	private List<String> ingredient;
 	public RecipeDTO() {
 	}
 
 	public RecipeDTO(Integer id, String name, String description, String image, Double rating, Integer numberOfRating,
 			Integer numberOfSaved, LocalDate submittedDate, List<String> tags, Integer servings,
-			Integer preparationTime, List<String> steps) {
+			Integer preparationTime, List<String> steps, Integer healthScore, Double calories, Double protein,
+			Double carbohydrate, Double sugar, Double sodium, Double fat, Double saturatedFat,String username,List<String>ingredient) {
 		this.id = id;
 		this.name = name;
 		this.description = description;
@@ -35,6 +47,24 @@ public class RecipeDTO {
 		this.servings = servings;
 		this.preparationTime = preparationTime;
 		this.steps = steps;
+		this.healthScore=healthScore;
+		this.calories=calories;
+		this.protein = protein;
+		this.carbohydrate = carbohydrate;
+		this.sugar = sugar;
+		this.sodium = sodium;
+		this.fat = fat;
+		this.saturatedFat = saturatedFat;
+		this.username=username;
+		this.ingredient=ingredient;
+	}
+
+	public List<String> getIngredient() {
+		return ingredient;
+	}
+
+	public void setIngredient(List<String> ingredient) {
+		this.ingredient = ingredient;
 	}
 
 	public Integer getId() {
@@ -131,6 +161,78 @@ public class RecipeDTO {
 
 	public void setSteps(List<String> steps) {
 		this.steps = steps;
+	}
+
+	public Integer getHealthScore() {
+		return healthScore;
+	}
+
+	public void setHealthScore(Integer healthScore) {
+		this.healthScore = healthScore;
+	}
+
+	public Double getCalories() {
+		return calories;
+	}
+
+	public void setCalories(Double calories) {
+		this.calories = calories;
+	}
+
+	public Double getProtein() {
+		return protein;
+	}
+
+	public void setProtein(Double protein) {
+		this.protein = protein;
+	}
+
+	public Double getCarbohydrate() {
+		return carbohydrate;
+	}
+
+	public void setCarbohydrate(Double carbohydrate) {
+		this.carbohydrate = carbohydrate;
+	}
+
+	public Double getSugar() {
+		return sugar;
+	}
+
+	public void setSugar(Double sugar) {
+		this.sugar = sugar;
+	}
+
+	public Double getSodium() {
+		return sodium;
+	}
+
+	public void setSodium(Double sodium) {
+		this.sodium = sodium;
+	}
+
+	public Double getFat() {
+		return fat;
+	}
+
+	public void setFat(Double fat) {
+		this.fat = fat;
+	}
+
+	public Double getSaturatedFat() {
+		return saturatedFat;
+	}
+
+	public void setSaturatedFat(Double saturatedFat) {
+		this.saturatedFat = saturatedFat;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
 	}
 
 }
